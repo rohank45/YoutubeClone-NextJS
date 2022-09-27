f5fd2e85e4494c5e3e35160e36336a017c00daaa
rohank45 <kurane.rohan99@gmail.com>
2022-09-27 19:21:28 +0530

    video card components ui issues fixed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost", "www.pexels.com"],
+    domains: ["localhost", "www.pexels.com", "i.ytimg.com"],
   },
 };
 
